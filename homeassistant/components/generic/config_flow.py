b5de45cb51e604d7cc5095b20d5398ee32e65507
Dave T <17680170+davet2001@users.noreply.github.com>
2020-11-13 22:06:59 +0000

    Add camera still image preview to config flow

--- homeassistant/components/generic/config_flow.py
+++ homeassistant/components/generic/config_flow.py
@@ -1,14 +1,18 @@
 """Config flow for generic (IP Camera)."""
+# TODO: enabling authentication on img preview seems to cause img load to fail
+# TODO: the above means we are currently exposing the preview image even to non-authenticated users.
+# TODO: we should really delete the HTTP view when we've finished with it but I don't know how.
 
 import imghdr
 import logging
 from typing import Any, Dict
 
+from aiohttp import web
 import av
 import voluptuous as vol
 
 from homeassistant import config_entries, data_entry_flow
-from homeassistant.components.camera import DEFAULT_CONTENT_TYPE
+from homeassistant.components.camera import DEFAULT_CONTENT_TYPE, CameraImageView
 from homeassistant.const import (
     CONF_AUTHENTICATION,
     CONF_NAME,
@@ -24,6 +28,7 @@ from .camera import GenericCamera
 # pylint: disable=unused-import
 from .const import (
     CONF_CONTENT_TYPE,
+    CONF_EDIT_NEEDED,
     CONF_FRAMERATE,
     CONF_LIMIT_REFETCH_TO_URL_CHANGE,
     CONF_STILL_IMAGE_URL,
@@ -31,6 +36,8 @@ from .const import (
     DEFAULT_NAME,
     DOMAIN,
 )
+
+SCAN_INTERVAL = 10
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -94,11 +101,41 @@ class GenericIPCamConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._errors = {}
 
         if user_input is not None:
+
+            self.user_input = user_input
             if await self._test_connection(user_input):
-                return self.async_create_entry(
-                    title=user_input[CONF_NAME], data=user_input
+                # if user_input.get(CONF_NAME) is not None:
+                #     return self.async_create_entry(
+                #         title=user_input[CONF_NAME], data=user_input
+                #     )
+                # Store the working data for the final step
+
+                # Register a temporary view so that we can preview the image
+                # at the confirmation step.
+                cam = GenericCamera(self.hass, self.user_input)
+                self.hass.http.register_view(CameraImagePreView(cam))
+                # hass.http.register_view(CameraMjpegStream(component))
+
+                return self.async_show_form(
+                    step_id="user_confirm",
+                    data_schema=vol.Schema(
+                        {
+                            vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+                            vol.Required(CONF_EDIT_NEEDED, default=False): bool,
+                        }
+                    ),
+                    errors=self._errors,
                 )
+
         else:
+            self.user_input = {}
+            self.user_input[CONF_NAME] = DEFAULT_NAME
+            self.user_input[CONF_AUTHENTICATION] = HTTP_BASIC_AUTHENTICATION
+            self.user_input[CONF_LIMIT_REFETCH_TO_URL_CHANGE] = False
+            self.user_input[CONF_CONTENT_TYPE] = DEFAULT_CONTENT_TYPE
+            self.user_input[CONF_FRAMERATE] = 2
+            self.user_input[CONF_VERIFY_SSL] = True
+
             user_input = {}
             user_input[CONF_NAME] = DEFAULT_NAME
             user_input[CONF_AUTHENTICATION] = HTTP_BASIC_AUTHENTICATION
@@ -109,41 +146,96 @@ class GenericIPCamConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(CONF_NAME, default=user_input[CONF_NAME]): str,
-                    vol.Required(
-                        CONF_STILL_IMAGE_URL,
-                        default=user_input.get(CONF_STILL_IMAGE_URL),
-                    ): str,
-                    vol.Optional(
-                        CONF_STREAM_SOURCE,
-                        default=user_input.get(CONF_STREAM_SOURCE, ""),
-                    ): str,
-                    vol.Optional(
-                        CONF_AUTHENTICATION, default=user_input[CONF_AUTHENTICATION]
-                    ): vol.In([HTTP_BASIC_AUTHENTICATION, HTTP_DIGEST_AUTHENTICATION]),
-                    vol.Optional(
-                        CONF_USERNAME,
-                        default=user_input.get(CONF_USERNAME, ""),
-                    ): str,
-                    vol.Optional(
-                        CONF_PASSWORD, default=user_input.get(CONF_PASSWORD, "")
-                    ): str,
-                    vol.Optional(
-                        CONF_LIMIT_REFETCH_TO_URL_CHANGE,
-                        default=user_input[CONF_LIMIT_REFETCH_TO_URL_CHANGE],
-                    ): bool,
-                    vol.Optional(
-                        CONF_CONTENT_TYPE, default=user_input[CONF_CONTENT_TYPE]
-                    ): str,
-                    vol.Optional(
-                        CONF_FRAMERATE, default=user_input[CONF_FRAMERATE]
-                    ): int,
-                    vol.Optional(
-                        CONF_VERIFY_SSL, default=user_input[CONF_VERIFY_SSL]
-                    ): bool,
-                }
-            ),
+            data_schema=self.get_main_input_schema(),
             errors=self._errors,
         )
+
+    async def async_step_user_confirm(
+        self, user_input: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
+        """Handle the confirmation step of the config flow."""
+        self._errors = {}
+
+        if user_input.get(CONF_EDIT_NEEDED, False):
+            return self.async_show_form(
+                step_id="user",
+                data_schema=self.get_main_input_schema(),
+                errors=self._errors,
+            )
+        if user_input is not None:
+            # FIXME if there is any way to unregister the http preview we should
+            # do it here e.g. self.hass.http.unregister_view(...)
+            return self.async_create_entry(
+                title=user_input[CONF_NAME], data=self.user_input
+            )
+
+    def get_main_input_schema(self):
+        """Get the schema for user data entry."""
+        return vol.Schema(
+            {
+                vol.Required(
+                    CONF_STILL_IMAGE_URL,
+                    default=self.user_input.get(CONF_STILL_IMAGE_URL),
+                ): str,
+                vol.Optional(
+                    CONF_STREAM_SOURCE,
+                    default=self.user_input.get(CONF_STREAM_SOURCE, ""),
+                ): str,
+                vol.Optional(
+                    CONF_AUTHENTICATION,
+                    default=self.user_input[CONF_AUTHENTICATION],
+                ): vol.In([HTTP_BASIC_AUTHENTICATION, HTTP_DIGEST_AUTHENTICATION]),
+                vol.Optional(
+                    CONF_USERNAME,
+                    default=self.user_input.get(CONF_USERNAME, ""),
+                ): str,
+                vol.Optional(
+                    CONF_PASSWORD, default=self.user_input.get(CONF_PASSWORD, "")
+                ): str,
+                vol.Optional(
+                    CONF_LIMIT_REFETCH_TO_URL_CHANGE,
+                    default=self.user_input[CONF_LIMIT_REFETCH_TO_URL_CHANGE],
+                ): bool,
+                vol.Optional(
+                    CONF_CONTENT_TYPE, default=self.user_input[CONF_CONTENT_TYPE]
+                ): str,
+                vol.Optional(
+                    CONF_FRAMERATE, default=self.user_input[CONF_FRAMERATE]
+                ): int,
+                vol.Optional(
+                    CONF_VERIFY_SSL, default=self.user_input[CONF_VERIFY_SSL]
+                ): bool,
+            }
+        )
+
+
+class CameraImagePreView(CameraImageView):
+    """Camera view to temporarily serve an image."""
+
+    url = "/api/camera_temp_proxy/generic_cf_camera_temp"
+    name = "api:camera:imgepreview"
+
+    def __init__(self, camera: GenericCamera) -> None:
+        """Initialize a basic camera view."""
+        self.camera = camera
+
+    async def get(self, request: web.Request) -> web.Response:
+        """Start a GET request."""
+        camera = self.camera
+
+        if camera is None:
+            raise web.HTTPNotFound()
+
+        # authenticated = (
+        #     request[KEY_AUTHENTICATED]
+        #     or request.query.get("token") in camera.access_tokens
+        # )
+
+        # if not authenticated:
+        #     raise web.HTTPUnauthorized()
+
+        if not camera.is_on:
+            _LOGGER.debug("Camera is off")
+            raise web.HTTPServiceUnavailable()
+
+        return await self.handle(request, camera)
