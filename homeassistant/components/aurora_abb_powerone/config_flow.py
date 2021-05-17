0d4ae8d80b4cc10d260da736c59d4b2edd921cad
Dave T <17680170+davet2001@users.noreply.github.com>
2021-05-17 21:48:10 +0000

    Add TCP mode to auroa_abb_powerone config flow.

--- homeassistant/components/aurora_abb_powerone/config_flow.py
+++ homeassistant/components/aurora_abb_powerone/config_flow.py
@@ -1,12 +1,12 @@
 """Config flow for Aurora ABB PowerOne integration."""
 import logging
 
-from aurorapy.client import AuroraError, AuroraSerialClient
+from aurorapy.client import AuroraError, AuroraSerialClient, AuroraTCPClient
 import serial.tools.list_ports
 import voluptuous as vol
 
 from homeassistant import config_entries, core
-from homeassistant.const import CONF_ADDRESS, CONF_PORT
+from homeassistant.const import CONF_ADDRESS, CONF_HOST, CONF_PORT, CONF_TYPE
 
 # pylint doesn't think DOMAIN is used.  But it is used!
 # Might be this bug? https://github.com/PyCQA/pylint/issues/3445
@@ -16,13 +16,43 @@ from .const import (
     ATTR_MODEL,
     ATTR_SERIAL_NUMBER,
     DEFAULT_ADDRESS,
+    DEFAULT_HOST,
     DEFAULT_INTEGRATION_TITLE,
+    DEFAULT_PORT,
+    DEFAULT_TYPE,
     DOMAIN,
     MAX_ADDRESS,
     MIN_ADDRESS,
 )
 
+CONF_TYPE_SERIAL = "Direct USB Connection/Serial RS485"
+CONF_TYPE_TCP = "TCP/IP to Serial Converter"
+
 _LOGGER = logging.getLogger(__name__)
+
+
+def schema_serial(comportslist, comportdefault):
+    """Create a flow schema for serial mode settings."""
+    config_options = {
+        vol.Required(CONF_PORT, default=comportdefault): vol.In([comportslist]),
+        vol.Required(CONF_ADDRESS, default=DEFAULT_ADDRESS): vol.In(
+            range(MIN_ADDRESS, MAX_ADDRESS + 1)
+        ),
+    }
+    return vol.Schema(config_options)
+
+
+def schema_tcp():
+    """Create a config flow schema for TCP mode settings."""
+    return vol.Schema(
+        {
+            vol.Required(CONF_HOST, default=DEFAULT_HOST): str,
+            vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+            vol.Required(CONF_ADDRESS, default=DEFAULT_ADDRESS): vol.In(
+                range(MIN_ADDRESS, MAX_ADDRESS + 1)
+            ),
+        }
+    )
 
 
 def validate_and_connect(hass: core.HomeAssistant, data):
@@ -53,6 +83,34 @@ def validate_and_connect(hass: core.HomeAssistant, data):
     return ret
 
 
+def validate_and_connect_tcp(hass: core.HomeAssistant, data):
+    """Validate the user input allows us to connect.
+
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    """
+    host = data[CONF_HOST]
+    port = data[CONF_PORT]
+    address = data[CONF_ADDRESS]
+    _LOGGER.debug("Intitialising connection to %s:%s [%s]", host, port, address)
+    ret = {}
+    ret["title"] = DEFAULT_INTEGRATION_TITLE
+    try:
+        client = AuroraTCPClient(host, port, address, timeout=1)
+        client.connect()
+        ret[ATTR_SERIAL_NUMBER] = client.serial_number()
+        ret[ATTR_MODEL] = f"{client.version()} ({client.pn()})"
+        ret[ATTR_FIRMWARE] = client.firmware(1)
+        _LOGGER.info("Returning device info=%s", ret)
+    except AuroraError as err:
+        _LOGGER.warning("Could not connect to device %s:%s [%s]", host, port, address)
+        raise err
+    finally:
+        client.close()
+
+    # Return info we want to store in the config entry.
+    return ret
+
+
 class AuroraABBConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Aurora ABB PowerOne."""
 
@@ -69,23 +127,50 @@ class AuroraABBConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialised by the user."""
 
         errors = {}
-        if self._comportslist is None:
-            comports = serial.tools.list_ports.comports(include_links=True)
-            comportslist = []
-            for port in comports:
-                comportslist.append(port.device)
-                _LOGGER.debug("COM port option: %s", port.device)
-            if len(comportslist) > 0:
-                defaultcomport = comportslist[0]
-            else:
-                _LOGGER.warning(
-                    "No com ports found.  Need a valid RS485 device to communicate"
-                )
-                return self.async_abort(reason="no_serial_ports")
-            self._comportslist = comportslist
-            self._defaultcomport = defaultcomport
+        if user_input is not None:
 
-        # Handle the initial step.
+            # print(f"user_input={user_input}")
+            if user_input.get(CONF_TYPE) == CONF_TYPE_SERIAL:
+                if self._comportslist is None:
+                    comports = serial.tools.list_ports.comports(include_links=True)
+                    comportslist = []
+                    for port in comports:
+                        comportslist.append(port.device)
+                        _LOGGER.debug("COM port option: %s", port.device)
+                    if len(comportslist) > 0:
+                        defaultcomport = comportslist[0]
+                    else:
+                        _LOGGER.warning(
+                            "No com ports found.  Need a valid RS485 device to communicate"
+                        )
+                        return self.async_abort(reason="no_serial_ports")
+                    self._comportslist = comportslist
+                    self._defaultcomport = defaultcomport
+
+                return self.async_show_form(
+                    step_id="user_serial",
+                    data_schema=schema_serial(self._defaultcomport, self._comportslist),
+                    errors=errors,
+                )
+            if user_input.get(CONF_TYPE) == CONF_TYPE_TCP:
+                return self.async_show_form(
+                    step_id="user_tcp",
+                    data_schema=schema_tcp(),
+                    errors=errors,
+                )
+        # If no user input, must be first pass through the config.  Show  initial form.
+        config_options = {
+            vol.Required(CONF_TYPE, default=DEFAULT_TYPE): vol.In(
+                [CONF_TYPE_SERIAL, CONF_TYPE_TCP]
+            ),
+        }
+        schema = vol.Schema(config_options)
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    async def async_step_user_serial(self, user_input=None):
+        """Handle step 2 (user has selected serial type then entered data)."""
+
+        errors = {}
         if user_input is not None:
             try:
                 info = await self.hass.async_add_executor_job(
@@ -115,15 +200,49 @@ class AuroraABBConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         error,
                     )
                     errors["base"] = "cannot_connect"
-        # If no user input, must be first pass through the config.  Show  initial form.
-        config_options = {
-            vol.Required(CONF_PORT, default=self._defaultcomport): vol.In(
-                self._comportslist
-            ),
-            vol.Required(CONF_ADDRESS, default=DEFAULT_ADDRESS): vol.In(
-                range(MIN_ADDRESS, MAX_ADDRESS + 1)
-            ),
-        }
-        schema = vol.Schema(config_options)
+        # If we get to here, show the form again (could be no data so far or error).
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema_serial(self._defaultcomport, self._comportslist),
+            errors=errors,
+        )
 
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+    async def async_step_user_tcp(self, user_input=None):
+        """Handle step 2 (user has selected serial type then entered data)."""
+
+        errors = {}
+        if user_input is not None:
+            try:
+                info = await self.hass.async_add_executor_job(
+                    validate_and_connect_tcp, self.hass, user_input
+                )
+                info.update(user_input)
+                # Bomb out early if someone has already set up this device.
+                device_unique_id = info["serial_number"]
+                await self.async_set_unique_id(device_unique_id)
+                self._abort_if_unique_id_configured()
+
+                return self.async_create_entry(title=info["title"], data=info)
+
+            except OSError as error:
+                if error.errno == 19:  # No such device.
+                    errors["base"] = "invalid_serial_port"
+            except AuroraError as error:
+                if "could not open port" in str(error):
+                    errors["base"] = "cannot_open_serial_port"
+                elif "No response after" in str(error):
+                    errors["base"] = "cannot_connect"  # could be dark
+                else:
+                    _LOGGER.error(
+                        "Unable to communicate with Aurora ABB Inverter at %s: %s %s",
+                        user_input[CONF_PORT],
+                        type(error),
+                        error,
+                    )
+                    errors["base"] = "cannot_connect"
+        # If we get to here, show the form again (could be no data so far or error).
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema_serial(self._defaultcomport, self._comportslist),
+            errors=errors,
+        )
